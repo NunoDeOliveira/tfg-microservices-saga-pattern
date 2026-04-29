0397fcbb2a15dc013a7f173fcd51e57c091441a6
vant <ndeoliveira.tlm@google.es>
2026-04-29 13:55:45 +0200

    Restaure pom.xml file

---
+++ api-gateway/src/main/java/com/tfg/api_gateway/controller/Fallback.java
@@ -0,0 +1,29 @@
+package com.tfg.api_gateway.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class Fallback {
+
+    @GetMapping("/fallback-production")
+    public Mono<ResponseEntity<String>> fallbackProduction() {
+        return Mono.just(
+                ResponseEntity
+                        .status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Production Service unavailable")
+        );
+    }
+
+    @GetMapping("/fallback-delivery")
+    public Mono<ResponseEntity<String>> fallbackDelivery() {
+        return Mono.just(
+                ResponseEntity
+                        .status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Delivery Service unavailable")
+        );
+    }
+}
---
+++ api-gateway/src/main/java/com/tfg/api_gateway/routing/GatewayRouting.java
@@ -0,0 +1,50 @@
+package com.tfg.api_gateway.routing;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class GatewayRouting {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+
+                // Production service
+                .route("production_route", r -> r
+                        .path("/production/**")
+                        .and()
+                        .method(HttpMethod.GET, HttpMethod.POST)
+                        .filters(f -> f
+                                .stripPrefix(1)
+                                .circuitBreaker(c -> {
+                                    c.setName("productionCircuitBreak");
+                                    c.setFallbackUri("forward:/fallback-production");
+                                }).addResponseHeader("X-Gateway", "Production-Service")
+                        )
+                        //.uri("http://production-service:8081")
+                        .uri("http://localhost:8081")
+                )
+
+                // Delivery service
+                .route("delivery_route", r -> r
+                        .path("/delivery/**")
+                        .and()
+                        .method(HttpMethod.GET, HttpMethod.POST)
+                        .filters(f -> f
+                                .stripPrefix(1)
+                                .circuitBreaker(c -> {
+                                    c.setName("deliveryCircuitBreak");
+                                    c.setFallbackUri("forward:/fallback-delivery");
+                                }).addResponseHeader("X-Gateway", "Delivery-Service")
+                        )
+                        //.uri("http://delivery-service:8082")
+                        .uri("http://localhost:8082")
+                )
+                .build();
+    }
+
+}
