0371d3dbdfef6742a27f52dad3089980fe599513
vant <ndeoliveira.tlm@google.es>
2026-06-02 19:11:23 +0200

    gateway routing remove method predicate

--- api-gateway/src/main/java/com/tfg/api_gateway/routing/GatewayRouting.java
+++ api-gateway/src/main/java/com/tfg/api_gateway/routing/GatewayRouting.java
@@ -3,21 +3,20 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 @Configuration
 public class GatewayRouting {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("production_route", r -> r.path("/production/**").and()
-                        .method(HttpMethod.GET, HttpMethod.POST, HttpMethod.DELETE)
+                .route("production_route", r -> r
+                        .path("/production/**")
                         .filters(f -> f.stripPrefix(1)
                                 .addResponseHeader("Gateway-Service", "Production-Service")
                         )
                         .uri("http://production-service:8081")
                 )
-                .route("delivery_route", r -> r.path("/delivery/**").and()
-                        .method(HttpMethod.GET, HttpMethod.POST, HttpMethod.DELETE)
+                .route("delivery_route", r -> r
+                        .path("/delivery/**")
                         .filters(f -> f.stripPrefix(1)
                                 .addResponseHeader("Gateway-Service", "Delivery-Service")
                         )
--- tests/final-test.sh
+++ tests/final-test.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-BASE_URL="http://localhost:8080"
+BASE_URL_PROD="http://13.134.79.165:8081"
+BASE_URL_DEL="http://13.134.79.165:8082"
+USERS=5
+REQUESTS=10
+NEXT_ID=1
+echo "========== test cancel every 5 productions"
 
-# Predice el próximo ID
-NEXT_ID=$(curl -s "$BASE_URL/production/productions" | python3 -c "import sys,json; data=json.load(sys.stdin); print(max([p['id'] for p in data])+1 if data else 1)")
-echo "Próximo ID predicho: $NEXT_ID"
-
-# Lanza POST y DELETE en paralelo
-curl -s -X POST "$BASE_URL/production/productions?amount=10" &
-sleep 0.15 && curl -s -X DELETE "$BASE_URL/production/productions/$NEXT_ID" &
-wait
-
-sleep 1
-curl -s "$BASE_URL/production/productions/$NEXT_ID"
-echo ""
+for i in $(seq 1 $REQUESTS); do
+    echo "=== Ronda $i ==="
+    for j in $(seq 1 4); do
+        curl -s -X POST "$BASE_URL_PROD/productions?amount=$((5 + RANDOM % 15))" &
+    done
+    curl -s -X POST "$BASE_URL_PROD/productions?amount=$((5 + RANDOM % 15))" &
+    sleep 0.15 && curl -s -X DELETE "$BASE_URL_PROD/productions/$((NEXT_ID + 4))" &
+    NEXT_ID=$((NEXT_ID + USERS))
+    wait
+    sleep 1
+done
 echo "Ending test ..."
-
