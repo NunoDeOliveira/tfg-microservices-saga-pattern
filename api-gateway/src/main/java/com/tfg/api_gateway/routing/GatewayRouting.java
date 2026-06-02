d5943f40150074eb5b279b75602e07b5235a4e0e
vant <ndeoliveira.tlm@google.es>
2026-06-02 12:40:32 +0200

    Delete circuit breaker

--- api-gateway/src/main/java/com/tfg/api_gateway/routing/GatewayRouting.java
+++ api-gateway/src/main/java/com/tfg/api_gateway/routing/GatewayRouting.java
@@ -1,41 +1,28 @@
 package com.tfg.api_gateway.routing;
-
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-
 @Configuration
 public class GatewayRouting {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-
         return builder.routes()
-                // Production service
                 .route("production_route", r -> r.path("/production/**").and()
                         .method(HttpMethod.GET, HttpMethod.POST, HttpMethod.DELETE)
-                        .filters(f -> f.stripPrefix(1).circuitBreaker(c -> {
-                                    c.setName("productionCircuitBreak");
-                                    c.setFallbackUri("forward:/fallback-production");
-                                }).addResponseHeader("Gateway-Service", "Production-Service")
+                        .filters(f -> f.stripPrefix(1)
+                                .addResponseHeader("Gateway-Service", "Production-Service")
                         )
                         .uri("http://production-service:8081")
-                        //.uri("http://localhost:8081")
                 )
-
-                // Delivery service
                 .route("delivery_route", r -> r.path("/delivery/**").and()
                         .method(HttpMethod.GET, HttpMethod.POST, HttpMethod.DELETE)
-                        .filters(f -> f.stripPrefix(1).circuitBreaker(c -> {
-                                    c.setName("deliveryCircuitBreak");
-                                    c.setFallbackUri("forward:/fallback-delivery");
-                                }).addResponseHeader("Gateway-Service", "Delivery-Service")
+                        .filters(f -> f.stripPrefix(1)
+                                .addResponseHeader("Gateway-Service", "Delivery-Service")
                         )
                         .uri("http://delivery-service:8082")
-                        //.uri("http://localhost:8082")
                 )
                 .build();
     }
-
 }
