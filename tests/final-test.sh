3b59dfa0274d4e83f966f846e02f12eff65c73a2
vant <ndeoliveira.tlm@google.es>
2026-06-02 18:26:12 +0200

    modify sleep to 200 ms

--- tests/final-test.sh
+++ tests/final-test.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 BASE_URL="http://localhost:8080"
 USERS=5
-REQUESTS=20
+REQUESTS=6
+
 
 echo "========== test cancel every 5 productions"
-
 NEXT_ID=1
+
 for i in $(seq 1 $REQUESTS); do
     echo "=== Ronda $i ==="
-    for j in $(seq 1 $USERS); do
+    # Bucle 1: 4 producciones normales
+    for j in $(seq 1 4); do
         curl -s -X POST "$BASE_URL/production/productions?amount=$((5 + RANDOM % 15))" &
     done
-wait
-sleep 1
+    # Bucle 2: 1 produccion + cancel en paralelo
+    curl -s -X POST "$BASE_URL/production/productions?amount=$((5 + RANDOM % 15))" &
+    sleep 0.2 && curl -s -X DELETE "$BASE_URL/production/productions/$((NEXT_ID + 4))" &
+    
+    NEXT_ID=$((NEXT_ID + USERS))
+    wait
+    sleep 1
 done
 
-echo "Ending test ..."
 
+
+echo "Ending test ..."
 
